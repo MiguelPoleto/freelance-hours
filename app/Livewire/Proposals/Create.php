ffc0cc7dd5d2f87f4515bb2913947bae83ade5ac
Miguel Poleto <miguelpoleto5@gmail.com>
2024-10-11 19:28:47 -0300

    Notificacoes

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Actions\arrangePositions;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
@@ -50,7 +52,7 @@ class Create extends Component
         });
 
 
-
+        $this->project->author()->notify(new NewProposal($this->project));
 
 
 
@@ -75,7 +77,9 @@ class Create extends Component
 
         if($otherProposal){
             $proposal->update(['position_status' => 'up']);
-            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+            $oProposal = Proposal::find($otherProposal->id);
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
         }
 
         arrangePositions::run($proposal->project_id);
